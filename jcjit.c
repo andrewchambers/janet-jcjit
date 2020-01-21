5a1d56f1cf89b4df93d5cad2102a549dbb658d0b
Andrew Chambers <ac@acha.ninja>
2020-01-21 18:13:30 +1300

    Fix some todos.

--- jcjit.c
+++ jcjit.c
@@ -1,39 +1,103 @@
+#include <errno.h>
 #include <janet.h>
 #include <libtcc.h>
+#include <sys/mman.h>
 
 #include "build/prelude.E.inc"
 
-static const JanetAbstractType function_type = {
-    "jcjit.function", NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+typedef struct {
+  Janet (*f)(int32_t, Janet *);
+  void *mem;
+  size_t n;
+} JitFunc;
+
+static int jitfunc_gc(void *p, size_t s) {
+  (void)s;
+  JitFunc *f = (JitFunc *)p;
+  if (f->mem == MAP_FAILED || f->mem == NULL)
+    return 0;
+  if (munmap(f->mem, f->n) != 0)
+    abort();
+  return 0;
+}
+
+static const JanetAbstractType function_type = {"jcjit.function",
+                                                jitfunc_gc,
+                                                NULL,
+                                                NULL,
+                                                NULL,
+                                                NULL,
+                                                NULL,
+                                                NULL,
+                                                NULL,
+                                                NULL};
+
+static void finalize_tcc_state(TCCState **s) {
+  if (*s)
+    tcc_delete(*s);
+}
+
+static void on_compile_error(void *opaque, const char *msg) {
+  Janet *errormsg = opaque;
+  *errormsg = janet_cstringv(msg);
+}
 
 static Janet jcjit_compile(int32_t argc, Janet *argv) {
   janet_fixarity(argc, 1);
-  const uint8_t *fsrc = janet_getcstring(argv, 0);
+  const uint8_t *fsrc = janet_getstring(argv, 0);
   fsrc = janet_formatc("%s\n#line 0 \"<cjit>\"\n%s", (char *)build_prelude_E,
                        fsrc);
 
-  TCCState *s = tcc_new();
-  if (!s)
+  TCCState **s = janet_smalloc(sizeof(struct TCCState *));
+  *s = tcc_new();
+  janet_sfinalizer(s, (void (*)(void *))finalize_tcc_state);
+
+  if (!*s)
     janet_panic("unable to create tcc state");
-  if (tcc_set_output_type(s, TCC_OUTPUT_MEMORY) != 0)
+
+  Janet errormsg = janet_wrap_nil();
+  tcc_set_error_func(*s, &errormsg, on_compile_error);
+
+  if (tcc_set_output_type(*s, TCC_OUTPUT_MEMORY) != 0)
     janet_panic("unable to set output type");
-  int r = tcc_compile_string(s, fsrc);
-  int n = tcc_relocate(s, NULL);
-  uint8_t *f = janet_abstract(&function_type, n + sizeof(void *));
-  tcc_relocate(s, f + sizeof(void *));
-  void *p = tcc_get_symbol(s, "f");
-  if (!p)
+
+  if (tcc_compile_string(*s, (const char *)fsrc) != 0)
+    janet_panicf("unable to compile function: %s",
+                 janet_checktype(errormsg, JANET_STRING)
+                     ? (char *)janet_unwrap_string(errormsg)
+                     : "unknown error");
+
+  JitFunc *f = janet_abstract(&function_type, sizeof(JitFunc));
+  f->f = NULL;
+  f->n = tcc_relocate(*s, NULL);
+  if (f->n <= 0)
+    janet_panic("unable to relocate function 1");
+  f->mem = mmap(NULL, f->n, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS,
+                -1, 0);
+
+  if (f->mem == MAP_FAILED)
+    janet_panicf("unable to map memory for jit function: %s", strerror(errno));
+
+  if (tcc_relocate(*s, f->mem) != 0)
+    janet_panic("unable to relocate function 2");
+
+  if (mprotect(f->mem, f->n, PROT_READ | PROT_EXEC) != 0)
+    janet_panic("unable to remote write permission on mapped memory");
+
+  f->f = tcc_get_symbol(*s, "f");
+  if (!f->f)
     janet_panic("jit must define a function named 'f'");
-  *(void **)f = p;
-  tcc_delete(s);
+
+  janet_sfree(s);
+
   return janet_wrap_abstract(f);
 }
 
 static Janet jcjit_call(int32_t argc, Janet *argv) {
   if (argc == 0)
     janet_panic("expected at least one argument");
-  void **f = janet_getabstract(argv, 0, &function_type);
-  return ((Janet(*)(int32_t, Janet *))(*f))(argc - 1, argv + 1);
+  JitFunc *f = janet_getabstract(argv, 0, &function_type);
+  return f->f(argc - 1, argv + 1);
 }
 
 static const JanetReg cfuns[] = {{"compile", jcjit_compile, NULL},
